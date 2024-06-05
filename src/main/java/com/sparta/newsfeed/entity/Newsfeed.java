3ad36eb1e56039aaebdec37ede0cb5f9c0dc1d15
haze <rmstn4233@gmail.com>
2024-06-05 17:01:07 +0900

    #6 뉴스피드CRUD 기능구현

--- src/main/java/com/sparta/newsfeed/entity/Newsfeed.java
+++ src/main/java/com/sparta/newsfeed/entity/Newsfeed.java
@@ -1,9 +1,5 @@
 package com.sparta.newsfeed.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +12,8 @@ public class Newsfeed {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private String title;
+	@Column
 	private String content;
 }
